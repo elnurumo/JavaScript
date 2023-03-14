7707c80a0f31f42c86d9a06b316ddc1fc7123bdc
elnurumo <elnurqasimov592@gmail.com>
2023-03-14 21:27:05 +0400

    Only JavaScript Done

---
+++ main.js
@@ -0,0 +1,40 @@
+const body = document.querySelector("body")
+
+body.innerHTML = 
+`
+<section>
+    <div class="contain big-box d-flex justify-content-center align-items-center mt-1 mb-4">
+        <h3 class=" text-white">960 x 360px</h3>
+    </div>
+</section>
+<section class="mb-4">
+    <div class="d-flex justify-content-between contain">
+        <div class="little-box_area">
+            <div class="little-box d-flex justify-content-center align-items-center">
+                <h3 class=" text-white">290 x 180px</h3>
+            </div>
+            <h6 class="text-muted mt-3"><i>Indonectetus facilis</i></h6>
+            <p class="text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, a repellendus?</p>
+            <div class="d-flex justify-content-end point w-100"><span class="text-warning">Read more <i class="fa-solid fa-angles-right my-icon-arrow"></i></span></div>
+        </div>
+
+        <div class="little-box_area">
+            <div class="little-box d-flex justify-content-center align-items-center">
+                <h3 class=" text-white">290 x 180px</h3>
+            </div>
+            <h6 class="text-muted mt-3"><i>Indonectetus facilis</i></h6>
+            <p class="text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, a repellendus?</p>
+            <div class="d-flex justify-content-end point"><span class="text-warning">Read more <i class="fa-solid fa-angles-right my-icon-arrow"></i></span></div>
+        </div>
+
+        <div class="little-box_area">
+            <div class="little-box d-flex justify-content-center align-items-center">
+                <h3 class=" text-white">290 x 180px</h3>
+            </div>
+            <h6 class="text-muted mt-3"><i>Indonectetus facilis</i></h6>
+            <p class="text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, a repellendus?</p>
+            <div class="d-flex justify-content-end point"><span class="text-warning">Read more <i class="fa-solid fa-angles-right my-icon-arrow"></i></span></div>
+        </div>
+    </div>
+</section>
+`
